0e3eb0a5c11532d0634e8acd084a73854c64e5c5
peter-mourad <pmourad772@gmail.com>
2022-09-03 22:10:26 +0200

    first commit

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 function RateHtmlFormatting(rate){
-    let html = `<span className="rate"><strong>${rate}</strong></span>\n`;
+    let html = `<span class="rate"><strong>${rate}</strong></span>\n`;
     for (let i = 0; i < 5; i++, rate--){
         if (rate >= 1) 
-            html += `\t\t\t\t\t\t\t<i className=fa fa-star"></i>\n`;
+            html += `\t\t\t\t\t\t\t<i class="fa fa-star"></i>\n`;
         else if (rate > 0) 
-            html += `\t\t\t\t\t\t\t<i className="fa fa-star-half-full"></i>\n`;
+            html += `\t\t\t\t\t\t\t<i class="fa fa-star-half-full"></i>\n`;
         else
-            html += `\t\t\t\t\t\t\t<i className="fa fa-star-o"></i>\n`;
+            html += `<i class="fa fa-star-o"></i>\n`;
     }
     return html;
 }
@@ -20,11 +20,11 @@ function Card({ data }) {
                 <div className="course-item">
                     <a href={course.link}>
                         <img src={course.image} alt=""></img>
-                        <i class="fa-solid fa-star"></i>
                         <p><strong>{course.title}</strong></p>
                         <p className="author">{course.author}</p>
-                        <div class="rate-section">
-                            {RateHtmlFormatting(course.rate)}
+                        <div className="rate-section"
+                            dangerouslySetInnerHTML={{ __html: RateHtmlFormatting(course.rate) }}
+                        >
                         </div>
                         <p className="price">E£${course.price}</p>
                     </a>
