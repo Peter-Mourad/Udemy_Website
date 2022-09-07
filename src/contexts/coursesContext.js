d1d8f8fc0ac4b961bc9741475f479c726bc46a14
peter-mourad <pmourad772@gmail.com>
2022-09-07 23:17:13 +0200

    created courses data context

---
+++ src/contexts/coursesContext.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react";
+
+const CoursesContext = React.createContext();
+
+function CoursesContextProvider({ children }) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+        const res = await fetch('http://localhost:8000/data');
+        const json = await res.json();
+        setData(json);
+    }
+    fetchData();
+  }, []);
+  
+  return (
+    <CoursesContext.Provider value={{ data }}>
+      { children }
+    </CoursesContext.Provider>
+  );
+}
+
+export { CoursesContext, CoursesContextProvider};
