b6869d01746c9f811027d6a09e46663309139572
peter-mourad <pmourad772@gmail.com>
2022-09-07 23:15:28 +0200

    finished courses sections

--- src/components/courses-section/CoursesSection.js
+++ src/components/courses-section/CoursesSection.js
@@ -10,25 +10,25 @@ const CoursesSection = ({ data }) => {
             <p>Choose from 185,000 online video courses with new additions published every month</p>
             <ul className="nav nav-tabs">
                 <li className="nav-item active" data-cont=".Python">
-                    <a className="nav-link" href=".Python">Python</a>
+                    <a className="nav-link" href="/">Python</a>
                 </li>
                 <li className="nav-item" data-cont=".Excel">
-                    <a className="nav-link" href=".Excel">Excel</a>
+                    <a className="nav-link" href="/">Excel</a>
                 </li>
                 <li className="nav-item" data-cont=".Web_Development">
-                    <a className="nav-link" href=".Web_Development">Web Development</a>
+                    <a className="nav-link" href="/">Web Development</a>
                 </li>
                 <li className="nav-item" data-cont=".JavaScript">
-                    <a className="nav-link" href=".JavaScript">javaScript</a>
+                    <a className="nav-link" href="/">javaScript</a>
                 </li>
                 <li className="nav-item" data-cont=".Data_Science">
-                    <a className="nav-link" href=".Data_Science">Data Science</a>
+                    <a className="nav-link" href="/">Data Science</a>
                 </li>
                 <li className="nav-item" data-cont=".AWS_Certification">
-                    <a className="nav-link" href=".AWS_Certificate">AWS Certification</a>
+                    <a className="nav-link" href="/">AWS Certification</a>
                 </li>
                 <li className="nav-item" data-cont=".Drawing">
-                    <a className="nav-link" href=".Drawing">Drawing</a>
+                    <a className="nav-link" href="/">Drawing</a>
                 </li>
             </ul>
             <CardsContainer data = { data }/>
