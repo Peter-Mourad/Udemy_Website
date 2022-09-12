f54e47ad8a767471e1122ecd63bbb99dd16a144b
peter-mourad <pmourad772@gmail.com>
2022-09-12 20:50:29 +0200

    created Rate component

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../globalStyle.css";
 
-const Rate = ({ rate }) => {
+const Rate = ({ rate , showRate}) => {
     const stars = [0, 0, 0, 0, 0];
     for (let i = 0; i < 5 && rate > i; i++){
         (rate >= (i + 1)) ? stars[i] = 1 : stars[i] = 0.5;
@@ -13,7 +13,7 @@ const Rate = ({ rate }) => {
     
     return (
         <div className="rate-section">
-            <span className="rate"><strong>{Math.round(rate * 10) / 10}</strong></span>
+            {(showRate ? <span className="rate"><strong>{Math.round(rate * 10) / 10}</strong></span> : '')}
             {stars.map(fill)}
         </div>
     );
