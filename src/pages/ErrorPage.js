323d2dc86ca01bb642aa20f5c58a9e293f3ab9f2
peter-mourad <pmourad772@gmail.com>
2022-09-15 15:34:37 +0200

    added popover component

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ErrorPage() {
+    return (
+        <img
+            style={{ width: "100%" }}
+            src="https://i.stack.imgur.com/6M513.png"
+            alt="error message">
+        </img>
+    );
+}
+
+export default ErrorPage;
